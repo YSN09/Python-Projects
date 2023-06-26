dce85a7d45e346d12b15269f6ab71047a41b9942
Yeshwant S Nair <129434247+YSN09@users.noreply.github.com>
2023-06-26 22:09:11 +0530

    Add files via upload

---
+++ Coin Distribution.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+final = []
+for y in range(10000):
+    tails = [0]
+    for x in range(10):
+        coin = np.random.randint(0,2)
+        if coin == 0:
+            tails.append(tails[x] + coin)
+        else:
+            tails.append(tails[x] + coin)
+    final.append(tails[-1])
+
+plt.hist(final, bins = 10)
+plt.show()
